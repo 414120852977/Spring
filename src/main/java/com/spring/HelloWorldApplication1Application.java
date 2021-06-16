28f6a1b72cc61fcb4ffdf923bf8260fca8f11c9e
Ashok Mane <ashokmane81800@gmail.com>
2021-06-16 06:53:20 +0530

    [ashok] added first spring boot application  and displaying hello from bridgelabz

---
+++ src/main/java/com/spring/HelloWorldApplication1Application.java
@@ -0,0 +1,14 @@
+package com.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication1Application.class, args);
+		System.out.println("Hello from bridgelabz");
+	}
+
+}
