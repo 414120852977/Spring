5434866bd87187d498acf8c00b0ec7e2ffa2abf2
Ashok Mane <ashokmane81800@gmail.com>
2021-06-16 08:22:08 +0530

    [ashok] added mvc controller to show on web page

--- src/main/java/com/spring/HelloWorldApplication1Application.java
+++ src/main/java/com/spring/HelloWorldApplication1Application.java
@@ -2,6 +2,9 @@ package com.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class HelloWorldApplication1Application {
@@ -9,6 +12,19 @@ public class HelloWorldApplication1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication1Application.class, args);
 		System.out.println("Hello from bridgelabz");
+	}
+	@Controller
+	public class HelloWebController {
+		@GetMapping("/HelloWorldApplication-1/src/main/resources/webapp/hello")
+		public String hello() {
+			return "hello";
+		}
+		
+		@GetMapping("/HelloWorldApplication-1/src/main/resources/webapp/message")
+		public String message (Model model) {
+			model.addAttribute("message", "This is a custom message");
+			return "message";
+		}
 	}
 
 }
